19f6a4e7233098bc53424def9e1e26982aef41f1
Ankit Kumar <89263177+akb6406@users.noreply.github.com>
2023-08-15 09:02:55 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Flip Bits/flip-bits.java
@@ -0,0 +1,56 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+	public static void main(String[] args) throws IOException
+	{
+	        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t =
+            Integer.parseInt(br.readLine().trim()); // Inputting the testcases
+        while(t-->0)
+        {
+            int n = Integer.parseInt(br.readLine().trim());
+            int a[] = new int[n];
+            String inputLine[] = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                a[i] = Integer.parseInt(inputLine[i]);
+            }
+            
+            Solution obj = new Solution();
+            System.out.println(obj.maxOnes(a, n));
+            
+        }
+	}
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+class Solution {
+
+    public static int maxOnes(int a[], int n) {
+        // Your code goes here
+        int count0=0;
+        int count1=0;
+        int maxlen=0;
+        for(int i=0;i<n;i++){
+            if(a[i]==1){
+                count1++;
+                if(count0!=0){
+                    count0--;
+                }
+            }else{
+                count0++;
+                maxlen= Math.max(maxlen, count0);
+            }
+        }
+        return maxlen+ count1;
+    }
+}
