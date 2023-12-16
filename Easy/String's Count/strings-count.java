dd0458649379d8f732b53cb4a12b7bcf73ab2dac
Ankit Kumar <89263177+akb6406@users.noreply.github.com>
2023-12-16 09:09:29 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/String's Count/strings-count.java
@@ -0,0 +1,35 @@
+//{ Driver Code Starts
+//Initial Template for Java
+import java.io.*;
+import java.util.*; 
+
+class GFG{
+    public static void main(String args[]) throws IOException { 
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+  
+        
+        int t = Integer.parseInt(br.readLine().trim()); 
+
+        while(t > 0){
+        	long n = Integer.parseInt(br.readLine().trim()); 
+            Solution ob = new Solution();
+            System.out.println(ob.countStr(n)); 
+            t--;
+        }
+    } 
+} 
+// } Driver Code Ends
+
+
+//User function Template for Java
+class Solution 
+{ 
+    static long countStr(long n)
+	{
+	    // code here
+	    if(n==1) return 3;
+	    if(n==2) return 8;
+	    long  ans = (n*n*n + 3*n +2)/2;
+	    return ans;
+	}
+} 
